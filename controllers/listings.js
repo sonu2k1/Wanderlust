37714b94b84cd42dcac5bf41d8e58c7dedeebbcb
sonu2k1 <sonuks654321@gmail.com>
2024-08-19 21:39:06 +0530

    maps

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/tilesets');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -30,7 +33,16 @@ module.exports.showListing = async (req, res) => {
   };
 
   module.exports.createListing = async (req, res, next) => {
-     let url = req.file.path;
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+       })
+       .send();
+
+     console.log(response.body.features[0].geometry);
+     res.send("done!");  
+
+    let url = req.file.path;
      let filename = req.file.filename;
     
     const newListing = new Listing(req.body.listing);
---
+++ public/js/map.js
@@ -0,0 +1,11 @@
+
+ 
+ mapboxgl.accessToken = mapToken;
+
+ const map = new mapboxgl.Map({
+  container: "map",
+  style:"mapbox://styles/mapbox/streets-v12",
+  center: [77.069710,  28.679079],
+  zoom: 9,
+ });
+
